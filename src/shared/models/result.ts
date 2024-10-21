2b913dc6c083e3175019de43df866b506a23f77e
DlopezS98 <01dlopezs98@gmail.com>
2024-10-20 23:24:53 -0600

    Result class improvement

--- src/shared/models/result.ts
+++ src/shared/models/result.ts
@@ -1,3 +1,5 @@
+import Guards from '../guards/common.guard';
+
 export default class Result<TValue> {
   constructor(public readonly success: boolean, public readonly message: string, public readonly value: TValue | null) {}
 
@@ -7,5 +9,12 @@ export default class Result<TValue> {
 
   static fail<TValue>(message: string): Result<TValue> {
     return new Result<TValue>(false, message, null);
+  }
+
+  /** Safe return the value, throws an error when value has null or undefined value */
+  getValue(): TValue {
+    if (Guards.isNullOrUndefined(this.value) || !this.success) throw new Error('Value null exception.');
+
+    return this.value;
   }
 }
